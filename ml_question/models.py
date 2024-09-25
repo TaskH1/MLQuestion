8801e0652ee977eddfdec979d3d9ed4fe18d73d8
Tasuku hideshima <hidey@MacBook-Pro-2.local>
2024-09-25 19:20:45 +0900

    Create Answer model

--- ml_question/models.py
+++ ml_question/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
+    correct_answer = models.CharField(default="Unknown")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -15,4 +16,11 @@ class Question(models.Model):
         """Return a string representation of the model."""
         return self.question_text
 
-    
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer_text = models.CharField()
+    is_correct = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.answer_text
