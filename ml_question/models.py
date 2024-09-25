193cae358417d0d15bcaba845aba46666903a542
Tasuku hideshima <hidey@MacBook-Pro-2.local>
2024-09-25 17:08:52 +0900

    Create superuser

--- ml_question/models.py
+++ ml_question/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # Define the table name
+    class Meta:
+        db_table = 'questions'
+        
+    def __str__(self):
+        """Return a string representation of the model."""
+        return self.text
+
+    
