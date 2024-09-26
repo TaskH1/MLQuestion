c716a824289a617670e6e9f8304f259d9fb2df2f
Tasuku hideshima <hidey@MacBook-Pro-2.local>
2024-09-26 17:39:00 +0900

    Add-Chapter

--- ml_question/models.py
+++ ml_question/models.py
@@ -2,15 +2,19 @@ from django.db import models
 
 # Create your models here.
 
+class Chapter(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.title
+
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    correct_answer = models.CharField(default="Unknown")
+    chapter = models.ForeignKey(Chapter, null=True, on_delete=models.CASCADE, related_name='questions')
+    question_text = models.CharField(max_length=300)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    # Define the table name
-    class Meta:
-        db_table = 'questions'
         
     def __str__(self):
         """Return a string representation of the model."""
@@ -18,9 +22,10 @@ class Question(models.Model):
 
 class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    answer_text = models.CharField()
+    answer_text = models.CharField(max_length=500)
     is_correct = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
+
 
     def __str__(self):
         return self.answer_text
