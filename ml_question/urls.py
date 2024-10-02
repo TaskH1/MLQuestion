b599780bb779f5d7ae5574598de2e9065795829c
Tasuku hideshima <hidey@MacBook-Pro-2.local>
2024-10-03 00:00:05 +0900

    Add Edit Chapter, Quesiton and Answer page

--- ml_question/urls.py
+++ ml_question/urls.py
@@ -4,21 +4,28 @@ from django.urls import path
 
 from . import views
 # distiguish this file from files of the same name in other apps within the project
+# this is a namespace
 app_name = "ml_question"
 
 urlpatterns = [
     # Home page
     path('', views.index, name='index'),
+
     # Chapter
-    # Chapter page where all chapters is showed
+    # Page where all chapters is showed
     path('chapters/', views.chapters, name='chapters' ),
     # Individual chapter page
     path('chapters/<int:chapter_id>', views.chapter, name='chapter'),
     # Page for adding a new chapter
     path('new_chapter/', views.new_chapter, name='new_chapter'),
+    # Page for editing chapter
+    path('edit_chapter/<int:chapter_id>', views.edit_chapter, name='edit_chapter'),
+    
     # Question
     # Individual quesition page
     path('chapters/<int:chapter_id>/<int:question_id>', views.question, name='question'),
-    # Page for adding a new question
+    # Page for adding a new question and an answer
     path('chapters/<int:chapter_id>/new_question', views.new_question, name='new_question'),
+    # Page for editing a question and an answer
+    path('edit_question/<int:question_id>', views.edit_question, name='edit_question'),
 ]
--- ml_question/views.py
+++ ml_question/views.py
@@ -11,6 +11,7 @@ def index(request):
     """Home page"""
     return render(request, 'ml_question/index.html')
 
+# Chapter
 def chapters(request):
     """Show all chapters"""
     chapters = Chapter.objects.order_by('number')
@@ -24,15 +25,6 @@ def chapter(request, chapter_id):
     context = {'chapter': chapter, 'questions': questions}
     return render(request, 'ml_question/chapter.html', context)
 
-def question(request, chapter_id, question_id):
-    """Show a single question"""
-    chapter = Chapter.objects.get(id=chapter_id)
-    question = Question.objects.get(id=question_id)
-    answer = Answer.objects.get(question_id=question_id)
-    context = {'chapter': chapter, 'question': question, 'answer': answer}
-    return render(request, 'ml_question/question.html', context)
-
-@csrf_exempt
 def new_chapter(request):
     """Create a new chapter"""
     if request.method != 'POST':
@@ -47,7 +39,32 @@ def new_chapter(request):
     context = {'form': form}
     return render(request, 'ml_question/new_chapter.html', context)
 
-@csrf_exempt
+def edit_chapter(request, chapter_id):
+    """Edit an existing chpater"""
+    chapter = Chapter.objects.get(id=chapter_id)
+
+    if request.method != 'POST':
+        # Initial request; pre-fill form with the current chpater.
+        form = ChapterForm(instance=chapter)
+    else:
+        # POST data submitted; process data.
+        form = ChapterForm(instance=chapter, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('ml_question:chapter', chapter_id=chapter.id)
+    
+    context = {'chapter': chapter, 'form': form}
+    return render(request, 'ml_question/edit_chapter.html', context)
+
+# Question
+def question(request, chapter_id, question_id):
+    """Show a single question"""
+    chapter = Chapter.objects.get(id=chapter_id)
+    question = Question.objects.get(id=question_id)
+    answer = Answer.objects.get(question_id=question_id)
+    context = {'chapter': chapter, 'question': question, 'answer': answer}
+    return render(request, 'ml_question/question.html', context)
+
 def new_question(request, chapter_id):
     """Create a new question and its answer"""
     chapter = Chapter.objects.get(id=chapter_id)
@@ -71,4 +88,32 @@ def new_question(request, chapter_id):
                 'answer_form': answer_form,
                 'chapter_id': chapter_id}
     return render(request, 'ml_question/new_question.html', context)
-        
+
+def edit_question(request, question_id):
+    """Edit a exsiting question and an answer"""
+    question = Question.objects.get(id=question_id)
+    answer = Answer.objects.get(question_id=question_id)
+
+    if request.method != 'POST':
+        question_form = QuestionForm(instance=question)
+        answer_form = AnswerForm(instance=answer)
+    else:
+        question_form = QuestionForm(instance=question, data=request.POST)
+        answer_form = AnswerForm(instance=answer, data=request.POST)
+        if question_form.is_valid() and answer_form.is_valid():
+            question_form.save()
+            answer_form.save()
+            return redirect('ml_question:question', chapter_id=question.chapter_id, question_id=question.id)
+        else:
+            print(question_form.errors)
+    
+    
+    context = {'question_form': question_form,
+               'answer_form': answer_form,
+               'question_id': question_id}
+    return render(request, 'ml_question/edit_question.html', context)
+
+
+
+
+    
