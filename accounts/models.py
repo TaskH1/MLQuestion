c6ad515bca0e4574f6f38ee0b174f9c6a8942a50
Tasuku hideshima <hidey@MacBook-Pro-2.local>
2024-10-08 16:39:10 +0900

    Keep important files, remove only cache files

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+from ml_question.models import Question, Answer
+
+class UserAnswer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    submitted_answer = models.TextField()
+    correct = models.BooleanField(default=False)
+
+    
