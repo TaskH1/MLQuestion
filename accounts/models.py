34d209c37e1b628440d0b815e77a091a5f3685e4
Tasuku hideshima <hidey@MacBook-Pro-2.local>
2024-10-09 13:40:21 +0900

    Create UserAnswer Model

--- accounts/models.py
+++ accounts/models.py
@@ -8,6 +8,9 @@ class UserAnswer(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     submitted_answer = models.TextField()
-    correct = models.BooleanField(default=False)
+    is_correct = models.BooleanField(default=False)
+    feedback = models.TextField(null=True, blank=True)
 
-    
+    def __str__(self):
+        return f"Answer by {self.user.username} for {self.question.question_text}"
+
